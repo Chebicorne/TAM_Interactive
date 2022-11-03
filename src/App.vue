46d1158554213f8d995936775129c8a1bec0be2f
Lenny ROLAND <thefunnygamers49@gmail.com>
2022-11-03 15:52:25 +0100

    Add routing & add Report page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="fixed bottom-0 left-0 right-0 h-28 bg-gray-200 flex justify-evenly items-center">
+    <router-link to="/map">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z" />
+      </svg>
+    </router-link>
+    <router-link to="/">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+      </svg>
+    </router-link>
+    <router-link to="/report">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5" />
+      </svg>
+    </router-link>
   </nav>
   <router-view/>
 </template>
 
 <style>
 nav a.router-link-exact-active {
+  @apply text-red-600;
 }
 </style>
---
+++ src/views/ReportView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="report">
+    <div class=""></div>
+    <div class="flex flex-col justify-center items-center h-screen">
+      <img class="w-64" src="@/assets/Panneau_travaux.png" alt="">
+      <div class="italic mt-6 text-gray-400 font-thin">Prochainement</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ReportVue',
+  components: {
+  }
+}
+</script>
