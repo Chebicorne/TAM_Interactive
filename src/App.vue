de4b0e86c2539e88cad37b470c6646e7ce4fbf32
Lenny ROLAND <thefunnygamers49@gmail.com>
2022-11-04 13:32:53 +0100

    add map view

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
+<script setup>
+
+</script>
+
+
 <template>
-  <nav class="fixed bottom-0 left-0 right-0 h-28 bg-gray-200 flex justify-evenly items-center">
+  <nav class="fixed bottom-0 left-0 right-0 h-28 bg-white flex justify-evenly items-center">
     <router-link to="/map">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
         <path stroke-linecap="round" stroke-linejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z" />
--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="about">
-    <h1>A proximité</h1>
-  </div>
+  <l-map style="height: calc(100vh - 112px);" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url"></l-tile-layer>
+  </l-map>
 </template>
+
+<script>
+import "leaflet/dist/leaflet.css"
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
+export default {
+  components: {
+    LMap,LTileLayer
+  },
+  data() {
+    return {
+      zoom: 15,
+      center: [51.505, -0.159],
+      url: 'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png',
+      geojson: {
+        type: "FeatureCollection",
+        features: [
+          // ...
+        ],
+      },
+      geojsonOptions: {
+        // Options that don't rely on Leaflet methods.
+      },
+    };
+  },
+};
+
+</script>
